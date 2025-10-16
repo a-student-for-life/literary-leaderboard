53af628455c8923d57271332dbac4ce0ea1521c4
Janice <38janice10b@gmail.com>
2025-10-16 17:15:18 +0530

    changed title

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -22,7 +22,7 @@ export default function Leaderboard() {
   return (
     <div className="min-h-screen px-8 py-10">
       <h1 className="text-3xl font-bold mb-8 text-center text-accent">
-        Literary Club Leaderboard ✍️
+        LitSoc Leaderboard ✍️
       </h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
